43aa8d0b0ca999465d3ef3086a12bd4841c34103
whitequark <whitequark@whitequark.org>
2016-02-15 19:23:01 +0000

    Fix deploy script to preserve proper permissions (outside .git).

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 ssh lab.m-labs.hk '\
   cd /var/lib/buildbot/masters/artiq && \
-  git fetch && git reset --hard origin/master && \
+  git fetch && sudo -u buildbot git reset --hard origin/master && \
   sudo service buildmaster restart || \
-  (tail -n50 twistd.log ; echo "Restart failed; see the reason above")'
+  (tail -n50 twistd.log ; echo "Restart failed, see the reason above")'

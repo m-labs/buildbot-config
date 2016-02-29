10e27741bfc3574deaea891b2222bbb3c3be46ef
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-02-29 21:59:40 +0800

    deploy.sh: prevent permissions/owner chaos in /var/lib/buildbot/masters/artiq/.git/objects
    @whitequark: I suggest changing .git/config to the github https url to avoid problems with public key authentication.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 ssh lab.m-labs.hk '\
   cd /var/lib/buildbot/masters/artiq && \
-  git fetch && sudo -u buildbot git reset --hard origin/master && \
+  sudo -u buildbot git fetch && sudo -u buildbot git reset --hard origin/master && \
   sudo service buildmaster restart || \
   (tail -n50 twistd.log ; echo "Restart failed, see the reason above")'

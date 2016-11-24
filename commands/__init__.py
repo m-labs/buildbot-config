35fa1945a92a5f593fd4b00617e23994179c97f8
whitequark <whitequark@whitequark.org>
2016-11-24 10:46:51 +0000

    Sleep before pinging, instead of using -w/-c ping options.
    These options don't work if there's a possibility that the host
    will not return ARP replies as then ping will exit immediately.

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,3 +1,4 @@
 from .lit_test import LitTestCommand
 from .coveralls import CoverallsCommand
 from .xilinx import XilinxCommand
+from .sleep import Sleep
---
+++ commands/sleep.py
@@ -0,0 +1,18 @@
+from buildbot.process.buildstep import BuildStep
+from buildbot.status.results import SUCCESS
+
+class Sleep(BuildStep):
+    """
+    A build step that does nothing for a predefined time.
+    """
+    parms = BuildStep.parms + ['delay']
+
+    delay = None # in seconds
+
+    def start(self):
+        self.step_status.setText(["sleeping", "%g sec" % self.delay])
+        reactor.callLater(self.delay, self.finished, SUCCESS)
+
+    def finished(self, results):
+        self.step_status.setText(["sleeped", "%g sec" % self.delay])
+        buildstep.BuildStep.finished(self, results)

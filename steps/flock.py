51bbf6c53d41ff7f46047417419af5567f0558c2
whitequark <whitequark@whitequark.org>
2017-11-26 23:15:18 +0000

    Refactor and document ARTIQ build steps.
    Don't waste time on e.g. uploading docs if we didn't even pass tests
    yet.

--- steps/flock.py
+++ steps/flock.py
@@ -61,5 +61,6 @@ class FunlockStep(BuildStep):
             self.step_status.setText(['unlocked', self.filename])
             self.finished(SUCCESS)
         else:
+            self.step_status.setText(['skipped', self.filename])
             self.finished(SKIPPED)
 

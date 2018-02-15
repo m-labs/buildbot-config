143a66d6bc2e076ccc131df04076b686e64c3396
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-02-15 22:12:17 +0800

    disable coveralls.io

--- steps/__init__.py
+++ steps/__init__.py
@@ -1,5 +1,4 @@
 from .lit_test import LitTestCommand
-from .coveralls import CoverallsCommand
 from .xilinx import XilinxCommand
 from .sleep import SleepStep
 from .flock import FlockStep, FunlockStep
